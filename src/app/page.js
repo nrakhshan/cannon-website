67e99ecee3a9088e8645aba0d738f81afe7598ea
Nusaiba Rakhshan <nrfpsgma@gmail.com>
2025-10-04 16:42:27 -0400

    testing if CI/CD is set up correctly

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-5xl font-bold text-center mt-20">Cannon Website</h1>
+      <h1 className="text-5xl font-bold text-center mt-20">The Cannon Website</h1>
       <p className="text-2xl text-center mt-5">Under construction. Check back soon.</p>
     </div>
   );
