53443aad8030b4dd9cbecd8e2f88aa6fe76abb31
Nusaiba Rakhshan <nrfpsgma@gmail.com>
2025-10-26 19:27:23 -0400

    Added some text to footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="">
       <div className="cantata text-[200px] leading-none uppercase text-left w-[65%] m-auto mt-25">The Cannon</div>
       <p className="afacad font-bold text-4xl text-center mt-15">Check back soon!</p>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <p className="text-center text-sm py-3">© 2025 The Cannon. All rights reserved.</p>
     </div>
   )
 }
