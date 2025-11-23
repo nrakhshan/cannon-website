f8002eca96460aa9300fb754966ec7766893f05a
Nusaiba Rakhshan <nrfpsgma@gmail.com>
2025-11-23 18:55:11 -0500

    merging dev w local

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
       <NavBar />
       <div className="cantata text-[200px] leading-none uppercase text-left w-[65%] m-auto mt-50">The Cannon</div>
       <p className="afacad font-bold text-4xl text-center mt-15">Home page</p>
-      <Footer />
       <Landing />
+      <Footer />
     </div>
   );
 }
