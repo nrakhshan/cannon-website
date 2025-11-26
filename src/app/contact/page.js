a94b728e69d06515e1a5f353ee6e76f968b8c0e1
lindenzheng <linden@parry8.com>
2025-11-26 15:26:21 -0500

    Contact Us page, mobile and desktop layouts.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,63 @@
 import NavBar from "@/components/NavBar";
+import ContactForm from "@/components/ContactForm";
+import ButtonRound from '@/components/ButtonRound';
+import Footer from "@/components/Footer";
 
 export default function Contact() {
   return (
     <div className="">
       <NavBar />
-      <div className="cantata text-[200px] leading-none uppercase text-left w-[65%] m-auto mt-50">The Cannon</div>
-      <p className="afacad font-bold text-4xl text-center mt-15">Contact Page</p>
+      <DesktopContact />
+      <MobileContact />
+      <Footer />
     </div>
   );
+}
+
+const DesktopContact = () => {
+  return (
+    <div className="hidden lg:block">
+      <div className="mt-15 w-full px-[6%] pt-24 pb-4 flex justify-between text-[18px]">
+          <div className="space-y-1 text-left">
+            <h1 className="cantata font-light text-4xl pb-4">Contact Us</h1>
+            <p className="afacad"><b>Instagram: </b>@cannon.mag</p>
+            <p className="afacad"><b>Discord: </b>discord.gg/FSwwhMqPPe</p>
+            <p className="afacad"><b>Email: </b>cannon@skule.ca</p>
+          </div>
+          <div className="w-[48%]">
+            <ContactForm />
+          </div>
+      </div>
+      <div className="w-full px-[6%] pb-24 space-y-4 text-left">
+          <h1 className="cantata font-light text-4xl">Get Involved</h1>
+          <p className="afacad text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          <div className="flex justify-start text-center mb-15">
+            <ButtonRound text="Join Us" isHighlight="true"/>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+const MobileContact = () => {
+  return (
+    <div className="block lg:hidden">
+      <div className="w-full px-[9%] pt-24 text-[18px]">
+          <div className="space-y-1 text-left pb-6">
+            <h1 className="cantata font-light text-4xl mt-15 pb-6">Contact Us</h1>
+            <p className="afacad"><b>Instagram: </b>@cannon.mag</p>
+            <p className="afacad"><b>Discord: </b>discord.gg/FSwwhMqPPe</p>
+            <p className="afacad"><b>Email: </b>cannon@skule.ca</p>
+          </div>
+          <ContactForm />
+          <div className="space-y-6 pt-36 pb-12 text-left">
+            <h1 className="cantata font-light text-4xl">Get Involved</h1>
+            <p className="afacad">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            <div className="w-full text-center pt-6 mb-15 flex justify-center">
+              <ButtonRound text="Join Us" isHighlight="true"/>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 const ContactForm = () => {
   return (
     <div>
-      <div className="py-2 px-4 mx-auto max-w-screen-md">
-            <form action="#" className="afacad text-base text-left font-normal">
+      <div className="mx-auto max-w-screen-md">
+            <form action="#" className="afacad text-base-left font-normal">
                 <div>
                     <label className="block my-2">
                         Name: </label>
-                    <input type="text" className="bg-secondary border border-base-300 rounded-full block w-full p-2.5" required />
+                    <input type="text" className="bg-secondary border border-base rounded-full block w-full p-2.5" required />
                 </div>
                 <div>
                     <label className="block my-2">
                         Email: </label>
-                    <input type="email" className="bg-background border border-base-300 rounded-full block w-full p-2.5" required />
+                    <input type="email" className="bg-background border border-base rounded-full block w-full p-2.5" required />
                 </div>
                 <div >
                     <label className="block my-2">
                         Message: </label>
-                    <textarea rows="6" className="block p-2.5 w-full bg-background rounded-2xl border border-base-300 " />
+                    <textarea rows="6" className="block p-2.5 w-full bg-background rounded-2xl border border-base" />
                 </div>
                 <button type="submit" className="mt-2 p-2 float-right font-semibold uppercase inline-flex items-center gap-2 group">
                     <span>Submit</span>
