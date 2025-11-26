a78812289514e82346bd3cc067b49032f393fafa
lindenzheng <linden@parry8.com>
2025-11-26 16:01:12 -0500

    Fixed mobile-desktop config.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,57 +7,25 @@ export default function Contact() {
   return (
     <div className="">
       <NavBar />
-      <DesktopContact />
-      <MobileContact />
-      <Footer />
-    </div>
-  );
-}
-
-const DesktopContact = () => {
-  return (
-    <div className="hidden lg:block">
-      <div className="mt-15 w-full px-[6%] pt-24 pb-4 flex justify-between text-[18px]">
-          <div className="space-y-1 text-left">
-            <h1 className="cantata font-light text-4xl pb-4">Contact Us</h1>
-            <p className="afacad"><b>Instagram: </b>@cannon.mag</p>
+      <div className="mt-15 w-full lg:px-[6%] pt-24 lg:pb-4 block lg:flex px-[9%] justify-between text-[18px]">
+          <div className="space-y-1 text-left pb-6">
+            <h1 className="cantata font-light text-4xl pb-6 lg:pb-4">Contact Us</h1>
+            <p className="afacad"><b>Instagram: </b>@thecannonuoft</p>
             <p className="afacad"><b>Discord: </b>discord.gg/FSwwhMqPPe</p>
             <p className="afacad"><b>Email: </b>cannon@skule.ca</p>
           </div>
-          <div className="w-[48%]">
+          <div className="lg:w-[48%]">
             <ContactForm />
           </div>
       </div>
-      <div className="w-full px-[6%] pb-24 space-y-4 text-left">
+      <div className="w-full px-[9%] lg:px-[6%] pb-12 lg:pb-24 pt-36 lg:pt-0 space-y-6 lg:space-y-4 text-left">
           <h1 className="cantata font-light text-4xl">Get Involved</h1>
           <p className="afacad text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-          <div className="flex justify-start text-center mb-15">
+          <div className="flex justify-center lg:justify-start text-center mb-15 pt-6 lg:pt-0">
             <ButtonRound text="Join Us" isHighlight="true"/>
           </div>
       </div>
+      <Footer />
     </div>
-  )
-}
-
-const MobileContact = () => {
-  return (
-    <div className="block lg:hidden">
-      <div className="w-full px-[9%] pt-24 text-[18px]">
-          <div className="space-y-1 text-left pb-6">
-            <h1 className="cantata font-light text-4xl mt-15 pb-6">Contact Us</h1>
-            <p className="afacad"><b>Instagram: </b>@cannon.mag</p>
-            <p className="afacad"><b>Discord: </b>discord.gg/FSwwhMqPPe</p>
-            <p className="afacad"><b>Email: </b>cannon@skule.ca</p>
-          </div>
-          <ContactForm />
-          <div className="space-y-6 pt-36 pb-12 text-left">
-            <h1 className="cantata font-light text-4xl">Get Involved</h1>
-            <p className="afacad">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-            <div className="w-full text-center pt-6 mb-15 flex justify-center">
-              <ButtonRound text="Join Us" isHighlight="true"/>
-            </div>
-          </div>
-      </div>
-    </div>
-  )
+  );
 }
