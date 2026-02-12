28e8ea791120c5324c6fac6e67dd8525ca7b8663
Nusaiba Rakhshan <nrfpsgma@gmail.com>
2026-02-11 22:04:01 -0500

    new implementation via mailtrap with HTML email template

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,16 +1,54 @@
 import { NextResponse } from 'next/server';
-import emailjs from '@emailjs/nodejs';
+const { MailtrapClient } = require("mailtrap");
+const { emailTemplateBuilder } = require('./emailTemplate')
+
+const TOKEN = process.env.MAILTRAP_KEY;
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@cannon.skule.ca",
+  name: "Cannon Webmaster",
+};
+
+const recipients = [
+  {
+    email: "webmaster@cannon.skule.ca",
+  }
+];
+
+const escapeHtml = (str = '') =>
+  String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 
 export async function POST(req) {
   try {
-    const { name, email, message } = await req.json();
+    let { name, email, message } = await req.json();
+    name = escapeHtml(name);
+    email = escapeHtml(email);
+    message = escapeHtml(message);
 
-    await emailjs.send(
-      process.env.SERVICE_ID,
-      process.env.TEMPLATE_ID,
-      { name, email, message },
-      { publicKey: process.env.PUBLIC_KEY }
+    const template = emailTemplateBuilder(
+      name,
+      email,
+      message
     );
+
+    client
+    .send({
+      from: sender,
+      to: recipients,
+      subject: `New message from ${name}`,
+      category: "Contact Form",
+      html: template,
+    })
+    .then(console.log, console.error);
 
     return NextResponse.json({ ok: true });
   } catch (err) {
